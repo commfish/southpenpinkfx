0059b11d659174d91d65b0bb89c95f7b3c76e17b
Power <sarah.power@alaska.gov>
2019-11-19 15:59:35 -0900

    initial commit

---
+++ code/southpenpinkfx2019.R
@@ -0,0 +1,55 @@
+# notes ----
+# forecasting using timeseries
+# sarah.power@alaska.gov
+# 11/19/2019
+
+# load ----
+library(fpp2)
+library(ggplot2)
+library(tidyverse)
+library(RDS)
+library(here)
+options(scipen=999)
+
+# data ----
+spenpinks2019 <- read.csv('data/spenpinks2019.csv') %>% 
+  select(year, run = post_june_harvest_escapement)
+
+#Check for NAs and fix in this small dataset
+spenpinks2019 %>% filter(run == "NA") 
+
+even <- spenpinks2019  %>% 
+  filter(year %% 2 == 0) %>% 
+  select(run) %>% 
+  as.ts()
+
+odd <- spenpinks2019 %>% 
+  filter(year %% 2 == 0) %>% 
+  as.ts()
+
+
+# analysis ----
+#autoplot(even)
+#ggAcf(even)
+#ggAcf(diff(even))
+#naive (last years value)
+fc <- fcn <- naive(even, h = 5)
+#simple exponential smoothing
+fc <- fcses <- ses(even, h = 1)
+#holt
+fc <- fch <- holt(y = even, h = 1,  exponential = FALSE) 
+#holt for the 2020 forecast don't use this method since residuals are not normal using the Ljung-Box test.
+fc <- fch <- holt(y = even, h = 1,  exponential = TRUE) 
+#damped holt done by Heather Finkle
+fc <- holt(y = even, h = 1, level = c(80, 80),  damped = TRUE, lambda = "auto") 
+fc <- holt(y = even, h = 1, damped = TRUE, lambda = "auto") 
+
+
+summary(fc)
+checkresiduals(fc)
+#accuracy(fc)
+autoplot(fc) + autolayer(fitted(fc))
+
+#Checking we have the right number of things also used inspection to check that the most recent values are accurate. 
+length(fitted(fc))
+length(even)
